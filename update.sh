49647a94ffb17195cd7812acb7f498fe4dcaab19
zhangweipu <884831651@qq.com>
2023-11-07 21:24:03 +0800

    add

--- update.sh
+++ update.sh
@@ -14,6 +14,7 @@ else
   echo "未找到端口 $PORT 的进程"
 fi
 
+echo "开始执行git命令"
 # 第三步：执行git pull命令
 # 进入您的Git仓库目录
 # 注意：在脚本中，请将以下路径替换为您的实际Git仓库路径
@@ -27,5 +28,5 @@ echo "已执行git pull"
 # 第四步：执行python命令
 # 假设您要运行一个名为your_script.py的Python脚本
 # 注意：在脚本中，请将以下命令替换为您要运行的实际Python命令
-python3 main.py
+nohup python3 main.py > output.log 2>&1 &
 echo "已执行Python命令"
